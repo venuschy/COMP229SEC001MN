3f0d43c7fab18161e54166681b495e6608fb5d2a
Hoi Ying CHAN <hchan134@my.centennialcollege.ca>
2023-06-07 23:08:12 -0400

    Assignment1 Version8

--- Assignment1/config/express.js
+++ Assignment1/config/express.js
@@ -3,36 +3,36 @@
    ID: 301272441
    Date: 9 June 2023 */
    
-var express = require('express'),
-    morgan = require('morgan'),
-    compress = require('compression'),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override');
+   var express = require('express'),
+   morgan = require('morgan'),
+   compress = require('compression'),
+   bodyParser = require('body-parser'),
+   methodOverride = require('method-override');
 
 module.exports = function () {
-    var app = express();
+   var app = express();
 
-    if (process.env.NODE_ENV === 'development') {
-        app.use(morgan('dev'));
+   if (process.env.NODE_ENV === 'development') {
+       app.use(morgan('dev'));
 
-    } else if (process.env.NODE_ENV === 'production') {
-        app.use(compress());
-    }
+   } else if (process.env.NODE_ENV === 'production') {
+       app.use(compress());
+   }
 
-    app.use(bodyParser.urlencoded({
-        extended: true
-    }));
+   app.use(bodyParser.urlencoded({
+       extended: true
+   }));
 
-    app.use(express.static('./public'));
+   app.use(express.static('./public'));
 
-    app.use(bodyParser.json());
-    app.use(methodOverride());
+   app.use(bodyParser.json());
+   app.use(methodOverride());
 
-    app.set('views', './app/views'),
-    app.set('view engine', 'ejs');
+   app.set('views', './app/views'),
+   app.set('view engine', 'ejs');
 
-    app.use('/', require('../app/routes/index.js'));
-    app.use(express.static("./node_modules/"));
-   
-    return app;
+   app.use('/', require('../app/routes/index.js'));
+   app.use(express.static("./node_modules/"));
+  
+   return app;
 }
